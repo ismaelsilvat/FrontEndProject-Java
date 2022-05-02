bae74e44b3348093bb6195900c7a24e47c0d5788
Bkuste2 <igorribeiro632@gmail.com>
2022-05-02 09:45:05 -0300

    Adding render data in the table

--- src/main/java/br/senai/controller/ClienteController.java
+++ src/main/java/br/senai/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.senai.controller;
 import br.senai.service.ClienteServiceImple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +13,8 @@ public class ClienteController {
     ClienteServiceImple clienteService;
 
     @GetMapping("/cliente/list")
-    public String findAll(){
+    public String findAll(Model model){
+        model.addAttribute("clientes", clienteService.findAll());
         return "cliente/list";
     }
 }
